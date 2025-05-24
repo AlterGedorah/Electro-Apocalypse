39cd4ef3b3fc618efc5433af6b44b82bac525a9c
AlterGedorah <gabrielcpercal@iskolarngbayan.pup.edu.ph>
2025-05-24 15:54:18 +0800

    Fixed the teleporting character

--- player.py
+++ player.py
@@ -1,66 +1,74 @@
+# player.py
 import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos, groups,obstacle_sprites):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('assets\sol-export.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
-        # self.image = pygame.transform.scale(pygame.image.load('assets\sol-export.png'),(16, 16))
-        self.obstacle_sprites = obstacle_sprites
-        self.hitbox = self.rect.inflate(0,-10)
+        self.image = pygame.image.load('assets/sol-export.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=pos)
 
+        # Create a smaller “hitbox” inside self.rect
+        self.hitbox = self.rect.inflate(0, -10)
+
+        self.obstacle_sprites = obstacle_sprites
         self.direction = pygame.math.Vector2()
         self.speed = 5
 
-       
-    def collision(self,direction):
+    def input(self):
+        keys = pygame.key.get_pressed()
+        # Vertical
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        # Horizontal
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def collision(self, direction):
+        """
+        Move hitbox, then check collisions against each obstacle's hitbox.
+        """
+
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
-                if sprite.rect.colliderect(self.hitbox):
-                    if self.direction.x > 0: #move right
+                # Compare tile.hitbox to player.hitbox
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0:  # moving right
                         self.hitbox.right = sprite.hitbox.left
-                    if self.direction.x < 0: #move left
+                    if self.direction.x < 0:  # moving left
                         self.hitbox.left = sprite.hitbox.right
 
         if direction == 'vertical':
             for sprite in self.obstacle_sprites:
                 if sprite.hitbox.colliderect(self.hitbox):
-                    if self.direction.y > 0: #down
-                        self.hitbox.bottom = sprite.hitbox.top    
-                    if self.direction.y < 0: #up
+                    if self.direction.y > 0:  # moving down
+                        self.hitbox.bottom = sprite.hitbox.top
+                    if self.direction.y < 0:  # moving up
                         self.hitbox.top = sprite.hitbox.bottom
 
-
-    def move(self,speed):
+    def move(self):
+        # Normalize diagonal speed
         if self.direction.magnitude() != 0:
-            self.direction = self.direction.normalize() # Normalizing the vector
-        
-        self.hitbox.x += self.direction.x * speed
+            self.direction = self.direction.normalize()
+
+        # Move on X, collide, then move on Y, collide
+        self.hitbox.x += self.direction.x * self.speed
         self.collision('horizontal')
-        self.hitbox.y += self.direction.y * speed
+        self.hitbox.y += self.direction.y * self.speed
         self.collision('vertical')
-        # self.rect.center += self.direction * speed
+
+        # NOW that hitbox is final, sync rect.center BEFORE drawing
         self.rect.center = self.hitbox.center
-
-
-
-    def input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]: #UP
-            self.direction.y = -1
-        elif keys[pygame.K_s]: #Down
-            self.direction.y = 1 
-        else:
-            self.direction.y = 0    
-        
-        if keys[pygame.K_a]:#Left
-            self.direction.x = -1
-        elif keys[pygame.K_d]: #Right
-            self.direction.x = 1 
-        else:
-            self.direction.x = 0 #if key is not pressed 
 
     def update(self):
         self.input()
-        self.move(self.speed)
+        self.move()
